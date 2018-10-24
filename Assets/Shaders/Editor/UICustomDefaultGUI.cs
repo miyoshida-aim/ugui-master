29b54adea872950f8d03110d256c9c2267a63748
miyoshida <miyoshida@aiming-inc.com>
2018-10-24 11:22:30 +0900

    ugui用シェーダーテスト追加

---
+++ Assets/Shaders/Editor/UICustomDefaultGUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace UGUIShader
+{
+    public class UICustomDefaultGUI : ShaderGUI
+    {
+        public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+        {
+            materialEditor.PropertiesDefaultGUI(properties);
+        }
+    }
+    
+}
+
